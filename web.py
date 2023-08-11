389b207498a8dfbec0275288e26227a376e04445
viveklistenus <viveklistenus@gmail.com>
2023-08-11 12:30:21 +0530

    Update web.py

--- web.py
+++ web.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import numpy as np
 from PIL import Image
-from model.generate_caption import *
+from generate_caption import *
 import os
 import gtts
 import pygame
